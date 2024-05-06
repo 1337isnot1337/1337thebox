f89871fe06c9f893e4b34941c9d595404acc5bcf
1337isnot1337 <164028474+1337isnot1337@users.noreply.github.com>
2024-05-06 12:44:56 -0500

    Update start.rs

--- start.rs
+++ start.rs
@@ -1,8 +1,7 @@
 use std::process::{Command, Stdio};
 use std::io::{BufRead, BufReader};
-
-
-
+use std::{thread, env};
+use std::time::Duration;
 
 
 fn main() {
@@ -12,12 +11,27 @@ fn main() {
     let pass = &args[2];
     let yourip = &args[3];
     let otherip = &args[4];
+    println!("{},{},{},{}", user, pass, yourip, otherip);
+    
+    pythonstart();
+    let pythonpid = pythonpidfind();
+    thread::sleep(Duration::from_secs(10));
+    pythonpidkill(pythonpid);
 
 }
 
+fn pythonpidkill(pid: i32) {
+    let output = Command::new("kill")
+        .args(&[&format!("{}", pid)])
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Error in killing process")
+        .wait()
+        .expect("Failed to wait for kill command");
+}
 
-fn pythonpidfind() {
-    // run the ps auxf command, listing all processes running
+fn pythonpidfind() -> i32 {
+
     let output = Command::new("ps")
         .args(&["auxf"])
         .stdout(Stdio::piped())
@@ -29,25 +43,23 @@ fn pythonpidfind() {
 
     for line in reader.lines() {
         let line = line.expect("Error reading line");
-        if line.contains(&format!("[p]ython3 -m http.server 55203")) {
+        if line.contains("[p]ython3 -m http.server 55203") {
             let parts: Vec<&str> = line.split_whitespace().collect();
             python_pid = parts[1].to_string();
-            break;
+            println!("The PID of the python process is {}", python_pid);
+            return python_pid.parse().expect("Failed to parse PID");
         }
     }
+    panic!("Python process not found");
 }
 
-
-use std::process::Command;
-
 fn pythonstart() {
-    let port = "55203"; // specify the port
+    let port = "55203"; 
 
-    // Spawn the Python server process in the background
+ 
     Command::new("python3")
         .args(&["-m", "http.server", port])
         .spawn()
         .expect("Failed to start Python server");
     println!("Python server started on port {}", port);
 }
-
