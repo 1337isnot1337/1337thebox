9cf0e36745f5fc0ad3e3f8f3fc1832b4d17f6091
1337isnot1337 <164028474+1337isnot1337@users.noreply.github.com>
2024-05-10 10:10:42 -0500

    Update message.sh

--- message.sh
+++ message.sh
@@ -1,39 +1,49 @@
 #!/bin/bash
 
-# Check for pass
-if [ -z "$1" ]; then
-    echo "Usage: $0 <root_password>"
-    exit 1
-fi
 
-# Set pass from arg
-root_password="$1"
+
+# Function to handle SIGINT signal
+sigint_handler() {
+    echo "Exiting..."
+    exit 1
+}
+
+# Function for error handling
+handle_error() {
+    local error_message="$1"
+    echo "Error: $error_message"
+}
+
+# Trap SIGINT signal and call the handler function
+trap sigint_handler SIGINT
 
 while true; do
     # Get the list of PIDs for non-root processes excluding the script's PID
-    pids=$(ps -e -o pid,cmd,user | grep -E '(/bin/)?(bash|ssh|sh)' | grep -v 'root' | grep -v $$ | awk '{print $1}')
+    pids=$(ps -e -o pid,user,cmd | grep -E '(/bin/)?(bash|ssh|sh|sshd)' | grep -v 'root' | grep -v $$ | grep -v $(echo $$) | awk '{print $1}')
+
+    if [ -z "$pids" ]; then
+        handle_error "No matching processes found"
+        sleep 5
+        continue
+    fi
 
     # Prompt user for message
     read -p "Enter a message to send: " message
 
+    if [ -z "$message" ]; then
+        handle_error "Message cannot be empty"
+        continue
+    fi
+
     # Iterate over PIDs
     for pid in $pids; do
-        # Send notice
-        echo "Message sent to pid $pid"
-
         # Execute GDB commands
-        gdb -p $pid >/dev/null 2>&1 <<EOF &
-            call system("echo .")
-            call system("echo Hello")
-            call system("echo I know this seems really weird but hear me out")
-            call system("echo The root password is $root_password")
-            call system("echo $message")
-            call system("echo I won’t waste your time anymore… but I bet you are curious! Go to https://github.com/1337isnot1337/1337thebox to learn more about what just happened. Have fun hacking :3")
-            quit
-EOF
+        sudo gdb -p $pid -ex "call system(\"echo $message\")" -ex quit >/dev/null 2>&1
+
+        if [ $? -ne 0 ]; then
+            handle_error "Failed to send message to pid $pid"
+        else
+            echo "Message sent to pid $pid"
+        fi
     done
-
-    # Sleepy time :3
-    sleep 30
-
 done
